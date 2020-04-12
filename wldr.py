ef06bf16330e0d49312faae634468c56423f0993
krashanoff <lkrashanoff2@gmail.com>
2020-04-12 01:24:14 -0700

    Efficient deletion

--- wldr.py
+++ wldr.py
@@ -9,13 +9,17 @@ import time
 
 import mgmt
 from stream import *
-from query import *
 
-CMD_PREFIX="//"
-CMD_SEPARATOR="="
+CMD_PREFIX="~"
+
 PREVIEW_COUNT=10
+MSG_LIMIT=2000
+DATETIME_FMT="%m/%d/%y|%H:%M"
 
-OPS=['d', 's']
+OK_EMOJI='\U00002705'
+NO_EMOJI='\U0000274C'
+REPEAT_EMOJI='\U0001F501'
+INPUT=[OK_EMOJI, NO_EMOJI, REPEAT_EMOJI]
 
 ABOUT=r"""
 こんにちはー
@@ -28,8 +32,8 @@ USAGE=r"**Usage message unavailable.**"
 logging.basicConfig(level=logging.INFO)
 cli = commands.Bot(command_prefix=CMD_PREFIX)
 
-async def pretty_print_msg(message=discord.Message):
-    return f'{message.author}@{message.created_at}: {message.content[:50]}...'
+def preview_messages(msgs):
+    return str('\n\n').join([f'{m.author}@{m.created_at} in #{m.channel}: {m.content[:50]}...' for m in msgs[:PREVIEW_COUNT]])[:1500]
 
 @cli.event
 async def on_ready():
@@ -45,84 +49,76 @@ def is_admin():
 async def about(ctx):
     await ctx.channel.send(ABOUT, delete_after=30)
 
-@cli.command(name='q')
+@cli.command(name='d')
 @is_admin()
 async def query(ctx, *args):
-    q = Query.from_args(ctx.channel, args)
-    if q is None:
-        await ctx.channel.send('Bad query structure!', delete_after=5)
-        return
+    status_message = await ctx.channel.send('Request received. Standby.')
 
-    status_message = await ctx.channel.send('Querying channel history...')
-    if q.run():
-        await status_message.edit(content=q.results, delete_after=30)
-    else:
-        await status_message.edit(content='Query failed!', delete_after=5)
+    before = None            # lower bound on date range
+    after = None             # upper bound on date range
+    channels = [ctx.channel] # a specific channel to search, if need be
+    author = ".*"            # author name regexp
+    q = ".*"                 # search expr
 
-@cli.command(name='s')
-@is_admin()
-async def sed(ctx, *args):
-    await ctx.channel.send(Query.from_args(ctx.channel, args))
-
-# sed-like edit of messages within a query.
-async def sed_mode(operation=str,
-                       channel=discord.TextChannel,
-                       user=discord.User,
-                       before=dt.datetime,
-                       after=dt.datetime,
-                       authors=str,
-                       search_exp=str,
-                       replace_exp=None):
-    m, msgs = await start_query(channel,
-                                before,
-                                after,
-                                lambda m: re.search(search_exp, m.content) and re.match(authors, str(m.author)))
-
-    # fail if no results.
-    if len(msgs) == 0:
-        await m.edit(content='No results found!', delete_after=5)
-        return
-
-    # handle preview of query.
-    if operation == 'q' or operation == 'query':
-        count = len(msgs) if len(msgs) < PREVIEW_COUNT else PREVIEW_COUNT
-        msgs.reverse()
-        preview_text = str('\n\n').join([await pretty_print_msg(m) for m in msgs[:count]])[:1500]
-        await m.edit(content=f'Preview of results for query [{after}, {before}], matching `{search_exp}`:\n```{preview_text}\n...```')
-        input_msg = await channel.send(f'What is your desired operation (from {OPS})?')
-
+    for a in args:
         try:
-            cmd_select = await cli.wait_for('message', timeout=30, check=lambda m: m.author == user and m.content in OPS)
-        except asyncio.TimeoutError:
-            await input_msg.delete()
-            await m.edit(content='No input detected.', delete_after=5)
-            return
-        else:
-            operation = cmd_select.content
+            separator = a.find('=')
+            arg = a[:separator]
+            val = a[separator + 1:]
+        except:
+            return None
 
-        await channel.delete_messages([input_msg, cmd_select])
-
-    # execute the transaction.
-    if operation == 'd' or operation == 'delete':
-        await m.edit(content='Deleting messsages.')
-        await mgmt.bulk_delete(channel, msgs)
-
-    elif operation == 's' or operation == 'replace':
-        # handle replacement.
-        if replace_exp is None:
-            input_msg = await channel.send(f'What is your replacement regex? For reference, your search expression is: \"{search_exp}\"')
-            try:
-                search_exp = await cli.wait_for('message', timeout=30, check=lambda m: m.author == user)
-            except asyncio.TimeoutError:
-                m.edit(content='No input detected.', delete_after=5)
-                return
+        if arg == 'before':
+            if val == 'now':
+                before = dt.datetime.utcnow()
             else:
-                search_exp = str(search_exp.content)
+                before = dt.datetime.strptime(val, DATETIME_FMT)
+        elif arg == 'after':
+            after = dt.datetime.strptime(val, DATETIME_FMT)
+        # TODO
+        elif arg == 'channels':
+            if val == 'all':
+                channels = ctx.guild.text_channels
+        elif arg == 'author':
+            author = val
+        elif arg == 'q':
+            q = val
+        
+    def pred(m):
+        return author == str(m.author) and re.search(q, str(m.content))
 
-        await m.edit(content=f'Editing messages to match provided regexp: {replace_exp}')
-        # TODO: replacement.
+    await status_message.edit(content='Querying channel history...')
 
-    await m.edit(content='Transaction completed successfully.', delete_after=5)
+    msgs = []
+    for c in channels:
+        [msgs.append(m) async for m in c.history(limit=MSG_LIMIT, before=before, after=after) if pred(m)]
+    msgs.reverse()
+    if len(msgs) == 0:
+        await status_message.edit(content='No results found!', delete_after=5)
+        return
+    else:
+        await status_message.edit(content=f'Query completed successfully. Previewing results:\n```{preview_messages(msgs)}```')
+
+    prompt = await ctx.channel.send('Ready to delete?')
+    await prompt.add_reaction(OK_EMOJI)
+    await prompt.add_reaction(NO_EMOJI)
+
+    try:
+        r, u = await cli.wait_for('reaction_add',
+                                  timeout=30,
+                                  check=lambda r, u: u == ctx.author and r.emoji in INPUT)
+    except asyncio.TimeoutError:
+        await prompt.edit(content='No reaction detected. Terminating transaction.', delete_after=5)
+    else:
+        await prompt.delete()
+        if r.emoji == OK_EMOJI:
+            await status_message.edit(content='Deleting messages...')
+            await mgmt.bulk_delete(channel, msgs)
+        else:
+            await status_message.edit(content='Terminating transaction.', delete_after=5)
+            return
+
+    await status_message.edit(content='Transaction completed successfully.', delete_after=5)
 
 """TODO: Currently broken.
 # handles starting and stopping of stream chats.
