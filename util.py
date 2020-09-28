b33196e757848794e88d5f5d07351d0431e24e26
krashanoff <lkrashanoff2@gmail.com>
2020-09-28 13:41:30 -0700

    Shuffle users.

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import asyncio
 import typing
 import discord
 from discord.ext import commands
+import random
 
 from constants import *
 from perms import *
@@ -30,14 +31,40 @@ class Util(commands.Cog):
         await ctx.message.delete(delay=TMPMSG_DEFAULT)
         await status.edit(content=f"{ctx.author.mention} deleted invite **{inv.id}**.", delete_after=TMPMSG_DEFAULT)
     
+    # count messages
     @commands.command(help="Count messages matching a regex")
-    async def count(self, ctx, *, pattern: typing.Optional[str]):
-        status = await ctx.send(f"Counting messages in this channel matching pattern `{pattern}`...")
+    async def count(self, ctx, chan: typing.Optional[discord.TextChannel], *, pattern: typing.Optional[str]):
+        status = await ctx.send(f"Counting messages in {chan or ctx.channel} matching pattern `{pattern}`...")
         count = 0
-        async for m in ctx.history():
+        async for m in (chan or ctx).history(limit=None):
             if re.search(pattern or ".*", m.content):
                 count += 1
         await status.edit(content=f"Found {count} messages matching your query.")
+
+    # shuffle users among a category of voice channels
+    # TODO: opt-in shuffle
+    @commands.command(help="Shuffle users about a category")
+    @is_admin()
+    async def shuf(self, ctx, *, chan: discord.CategoryChannel):
+        status = await ctx.send(f"Okay, I'll shuffle users amongst {chan.name}.")
+        random.seed()
+        while True:
+            await status.edit(content="Waiting for your reaction...")
+            await status.add_reaction(OK_EMOJI)
+            await status.add_reaction(NO_EMOJI)
+            r, _ = await self.bot.wait_for("reaction_add", check=lambda r,u: u == ctx.author)
+            if r.emoji == NO_EMOJI:
+                break
+
+            await status.edit(content="Shuffling...")
+
+            # shuffle the lads
+            for m in [ m for c in chan.voice_channels for m in c.members ]:
+                await m.move_to(random.choice(chan.voice_channels))
+
+            await status.edit(content="Shuffled!")
+            await status.clear_reaction(OK_EMOJI)
+        await status.edit(content="Aborted.", delete_after=TMPMSG_DEFAULT)
 
     # bulk deletion tool
     @commands.command(name="d", help="Delete messages")
